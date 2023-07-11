e82c9ea3b6595b48b1a3b57b3b73a109abaec03f
StefanusLuigiMarcellino <luigimarcellino2003@gmail.com>
2023-07-11 14:03:46 +0700

    deskripsi home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,137 +8,145 @@
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">What is Credit Card Default?</h1>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus explicabo natus magni. Minima sint itaque, omnis nobis sunt at assumenda laborum nisi. Odio officia reiciendis, minus iusto similique mollitia cupiditate voluptas nulla repudiandae eveniet excepturi nihil. Doloribus optio incidunt ipsam aliquid! Unde repellat quod minima iure magnam ea dolorem impedit aspernatur? Ratione, voluptates ullam natus numquam nam impedit hic quia explicabo itaque ex aspernatur inventore ea architecto sunt non assumenda quis perspiciatis esse magni tenetur quibusdam quod deserunt. Unde saepe quibusdam perspiciatis illum tempora iste, rem at sequi beatae! Illum incidunt quibusdam animi eaque molestias consequatur eos doloribus nesciunt minima eveniet architecto laboriosam placeat error, ducimus labore fugiat distinctio corporis porro itaque maiores dolores ipsum voluptas. Ratione animi, omnis exercitationem delectus odio beatae esse corporis in laborum expedita, aliquam, sint ad necessitatibus rerum vel a temporibus accusamus saepe eveniet perspiciatis vero! Adipisci, dignissimos non commodi distinctio, suscipit, blanditiis facere temporibus culpa voluptas quod ipsam quo doloremque. Sit sunt alias aperiam ea cupiditate consequatur perspiciatis iusto ipsum quod, libero beatae porro quos iure sapiente molestiae voluptas vitae. Alias, quaerat necessitatibus amet laudantium corrupti sunt architecto est et reiciendis nihil possimus iure eum dicta minima nisi non consequatur hic sequi quibusdam perspiciatis corporis ad! Maiores, facere vero? Perferendis, nobis quae ex sint quaerat dignissimos cumque autem voluptas expedita architecto magnam quidem earum error laboriosam et aperiam dolorum saepe placeat voluptatibus velit ut perspiciatis. Sunt est ut sed vel quae dolore debitis modi atque tempore, vitae incidunt eum dolorem sint dignissimos excepturi quos error nobis! Ratione culpa necessitatibus aliquid delectus dolor! Distinctio, voluptatibus saepe unde velit aspernatur incidunt quae quo fugit eligendi iste, consequatur porro eos reprehenderit optio suscipit aut! Eos odio nobis quasi quas iusto sint accusantium, nam corporis vitae vel dicta praesentium beatae nostrum ipsum quidem necessitatibus totam aliquid quod sit.</p>
+        <p>
+            Saat ini transaksi dengan metode pembayaran cashless terutama penggunaan kartu kredit sangat membludak karena kemudahannya dalam melakukan transaksi dan banyaknya promo yang menguntungkan dan menggiurkan bagi para pengguna kartu kredit.
+            Akibatnya, banyak orang yang tertarik untuk menggunakan kartu kredit.
+            Namun, para pengguna kartu kredit sering kali melakukan transaksi dengan metode pembayaran jenis ini tanpa adanya kontrol dalam penggunaan kartu kredit karena mengejar promo dan sebagainya.
+            Ketidakmampuan dalam mengontrol penggunaan kartu kredit mengakibatkan kerugian pada perusahaan maupun instansi finansial karena banyaknya orang yang tidak mampu membayar tagihan kartu kredit yang mereka gunakan.
+            Perusahaan maupun instansi tentu tidak dengan sembarangan memberikan pinjaman kepada customernya, namun syarat yang dibutuhkan dalam mendaftarkan pembuatan kartu kredit tidak mampu memprediksi secara tepat kemampuan bayar tagihan kartu kredit tersebut.
+            Hal ini juga diakibatkan dari kesalahan manusia (Human Error) yang tidak mampu secara akurat memprediksi hal tersebut. Sehingga perusahaan tetap memberikan pinjaman kepada customer yang ingin membuat kartu kredit tanpa dapat memastikan apakah orang tersebut mampu membayar tagihan kartu kreditnya atau tidak.
+        </p>
 
         <div
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Our Dataset</h1>
         </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit ducimus exercitationem ab possimus similique? Incidunt hic libero consequatur assumenda laudantium magnam eligendi sequi ratione vel at tempora dolorem veritatis, eius illum esse tenetur rerum harum beatae perspiciatis debitis accusamus quis minus? Odio quaerat quas officiis deleniti ab eum amet odit?</p>
-
+        <p>
+            Dataset ini berasal dari Kaggle yang diunggah pada tahun 2019 tanpa diketahui dataset ini berasal dari negara atau sumber yang jelas dengan 121 atribut dan lebih dari 300.000 data.
+        </p>
         <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
+                  <th scope="col">TARGET</th>
+                  <th scope="col">NAME_CONTRACT_TYPE</th>
+                  <th scope="col">CODE_GENDER</th>
+                  <th scope="col">AMT_INCOME_TOTAL</th>
+                  <th scope="col">AMT_CREDIT</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
+                  <td>1</td>
+                  <td>Cash Loans</td>
+                  <td>M</td>
+                  <td>202500</td>
+                  <td>406597.5</td>
                 </tr>
                 <tr>
-                  <td>1,002</td>
-                  <td>placeholder</td>
-                  <td>irrelevant</td>
-                  <td>visual</td>
-                  <td>layout</td>
+                  <td>0</td>
+                  <td>Cash Loans</td>
+                  <td>F</td>
+                  <td>270000</td>
+                  <td>1293502.5</td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>data</td>
-                  <td>rich</td>
-                  <td>dashboard</td>
-                  <td>tabular</td>
+                  <td>0</td>
+                  <td>Revolving loans</td>
+                  <td>M</td>
+                  <td>67500</td>
+                  <td>135000</td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>information</td>
-                  <td>placeholder</td>
-                  <td>illustrative</td>
-                  <td>data</td>
+                  <td>1</td>
+                  <td>Revolving loans</td>
+                  <td>M</td>
+                  <td>180000</td>
+                  <td>540000</td>
                 </tr>
                 <tr>
-                  <td>1,004</td>
-                  <td>text</td>
-                  <td>random</td>
-                  <td>layout</td>
-                  <td>dashboard</td>
+                  <td>0</td>
+                  <td>Revolving loans</td>
+                  <td>F</td>
+                  <td>157500</td>
+                  <td>315000</td>
                 </tr>
                 <tr>
-                  <td>1,005</td>
-                  <td>dashboard</td>
-                  <td>irrelevant</td>
-                  <td>text</td>
-                  <td>placeholder</td>
+                  <td>0</td>
+                  <td>Cash Loans</td>
+                  <td>M</td>
+                  <td>90000</td>
+                  <td>336150</td>
                 </tr>
                 <tr>
-                  <td>1,006</td>
-                  <td>dashboard</td>
-                  <td>illustrative</td>
-                  <td>rich</td>
-                  <td>data</td>
+                  <td>1</td>
+                  <td>Cash Loans</td>
+                  <td>F</td>
+                  <td>135000</td>
+                  <td>495216</td>
                 </tr>
                 <tr>
-                  <td>1,007</td>
-                  <td>placeholder</td>
-                  <td>tabular</td>
-                  <td>information</td>
-                  <td>irrelevant</td>
+                  <td>1</td>
+                  <td>Revolving loans</td>
+                  <td>F</td>
+                  <td>73341</td>
+                  <td>135000</td>
                 </tr>
                 <tr>
-                  <td>1,008</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
+                  <td>1</td>
+                  <td>Cash Loans</td>
+                  <td>M</td>
+                  <td>121500</td>
+                  <td>263686.5</td>
                 </tr>
                 <tr>
-                  <td>1,009</td>
-                  <td>placeholder</td>
-                  <td>irrelevant</td>
-                  <td>visual</td>
-                  <td>layout</td>
+                  <td>0</td>
+                  <td>Cash Loans</td>
+                  <td>F</td>
+                  <td>202500</td>
+                  <td>225000</td>
                 </tr>
                 <tr>
-                  <td>1,010</td>
-                  <td>data</td>
-                  <td>rich</td>
-                  <td>dashboard</td>
-                  <td>tabular</td>
+                  <td>0</td>
+                  <td>Cash Loans</td>
+                  <td>F</td>
+                  <td>157500</td>
+                  <td>450000</td>
                 </tr>
                 <tr>
-                  <td>1,011</td>
-                  <td>information</td>
-                  <td>placeholder</td>
-                  <td>illustrative</td>
-                  <td>data</td>
+                  <td>0</td>
+                  <td>Cash Loans</td>
+                  <td>M</td>
+                  <td>202500</td>
+                  <td>526491</td>
                 </tr>
                 <tr>
-                  <td>1,012</td>
-                  <td>text</td>
-                  <td>placeholder</td>
-                  <td>layout</td>
-                  <td>dashboard</td>
+                  <td>1</td>
+                  <td>Cash Loans</td>
+                  <td>M</td>
+                  <td>225000</td>
+                  <td>1019205</td>
                 </tr>
                 <tr>
-                  <td>1,013</td>
-                  <td>dashboard</td>
-                  <td>irrelevant</td>
-                  <td>text</td>
-                  <td>visual</td>
+                  <td>0</td>
+                  <td>Revolving loans</td>
+                  <td>F</td>
+                  <td>112500</td>
+                  <td>540000</td>
                 </tr>
                 <tr>
-                  <td>1,014</td>
-                  <td>dashboard</td>
-                  <td>illustrative</td>
-                  <td>rich</td>
-                  <td>data</td>
+                  <td>0</td>
+                  <td>Revolving loans</td>
+                  <td>F</td>
+                  <td>225000</td>
+                  <td>675000</td>
                 </tr>
                 <tr>
-                  <td>1,015</td>
-                  <td>random</td>
-                  <td>tabular</td>
-                  <td>information</td>
-                  <td>text</td>
+                  <td>1</td>
+                  <td>Revolving loans</td>
+                  <td>M</td>
+                  <td>270000</td>
+                  <td>810000</td>
                 </tr>
               </tbody>
             </table>
